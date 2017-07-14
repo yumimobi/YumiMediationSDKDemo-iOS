c972177e96b4699f2398650d51fe26504d1c8c77
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-07-14 10:43:30 +0800

    Test IBOutlet

--- YumiMediationSDKDemo/YumiMobiAppViewController.m
+++ YumiMediationSDKDemo/YumiMobiAppViewController.m
@@ -10,6 +10,8 @@
 
 @interface YumiMobiAppViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *yumiMediationButton;
+
 @end
 
 @implementation YumiMobiAppViewController
@@ -17,21 +19,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.yumiMediationButton.layer.borderWidth = 1;
+    
+    self.yumiMediationButton.layer.borderColor  = [UIColor whiteColor].CGColor;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)clickMetation:(UIButton *)sender {
+    
 }
-*/
 
 @end
